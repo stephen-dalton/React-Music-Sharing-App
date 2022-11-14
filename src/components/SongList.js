f8790e8cd22b5e935cb3f3eef3ed446ca4e25c0b
Stephen Dalton <daltsdev@icloud.com>
2022-11-14 19:46:29 +0000

    introduce pause play dispatcher types

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,20 +1,35 @@
-import { useSubscription } from '@apollo/react-hooks';
-import { PlayCircleFilledWhite, Save } from '@mui/icons-material';
-import { Card, CardActions, CardContent, CardMedia, CircularProgress, IconButton, Typography } from '@mui/material';
-import React from 'react';
-import { GET_SONGS } from '../graphql/subscriptions';
+import { useSubscription } from "@apollo/react-hooks";
+import { PlayCircleFilledWhite, Save } from "@mui/icons-material";
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  CircularProgress,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import { GET_SONGS } from "../graphql/subscriptions";
 
 export default function SongList() {
   const { data, loading, error } = useSubscription(GET_SONGS);
 
-  console.log(data, loading, error)
-
   if (loading) {
     return (
-      <div style={{ display: 'flex', alignItems: 'center', flexDirection: 'column', justifyContent: 'center', mt: 50, lt: 50 }}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          justifyContent: "center",
+          mt: 50,
+          lt: 50,
+        }}
+      >
         <CircularProgress />
       </div>
-    )
+    );
   }
 
   if (error) {
@@ -27,34 +42,50 @@ export default function SongList() {
         <Song key={song.id} song={song} />
       ))}
     </div>
-  )
+  );
 }
 
 function Song({ song }) {
   const { title, artist, thumbnail } = song;
   return (
     <Card sx={{ margin: 3 }}>
-      <div style={{ display: 'flex', alignItems: "center" }}>
-        <CardMedia component="img" image={thumbnail} alt="Song Cover" sx={{ width: 140, height: 140, objectFit: 'cover' }} />
-        <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <CardMedia
+          component="img"
+          image={thumbnail}
+          alt="Song Cover"
+          sx={{ width: 140, height: 140, objectFit: "cover" }}
+        />
+        <div
+          style={{
+            width: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {title}
             </Typography>
-            <Typography gutterBottom variant="body1" component="p" color="text.alt">
+            <Typography
+              gutterBottom
+              variant="body1"
+              component="p"
+              color="text.alt"
+            >
               {artist}
             </Typography>
           </CardContent>
           <CardActions>
-            <IconButton size="medium" sx={{ color: 'text.alt' }}>
+            <IconButton size="medium" sx={{ color: "text.alt" }}>
               <PlayCircleFilledWhite />
             </IconButton>
-            <IconButton size="medium" sx={{ color: 'text.white' }}>
+            <IconButton size="medium" sx={{ color: "text.white" }}>
               <Save />
             </IconButton>
           </CardActions>
         </div>
       </div>
     </Card>
-  )
+  );
 }
--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,42 +1,92 @@
-import { useTheme } from '@mui/material/styles';
-import { PlayArrow, SkipNext, SkipPrevious } from '@mui/icons-material'
-import { Card, CardContent, Typography, IconButton, Slider, CardMedia } from '@mui/material'
-import React from 'react'
-import QueuedSongList from './QueuedSongList'
+import React, { useContext } from "react";
+import { useTheme } from "@mui/material/styles";
+import { PlayArrow, SkipNext, SkipPrevious, Pause } from "@mui/icons-material";
+import {
+  Card,
+  CardContent,
+  Typography,
+  IconButton,
+  Slider,
+  CardMedia,
+} from "@mui/material";
+import QueuedSongList from "./QueuedSongList";
+import { SongContext } from "../App";
 
 export default function SongPlayer() {
+  const { state, dispatch } = useContext(SongContext);
   const theme = useTheme();
+  const handleToggleClick = () => {
+    dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
+  };
   return (
     <>
-      <Card variant='outlined' sx={{ display: 'flex', justifyContent: 'space-between' }}>
-        <div style={{ display: 'flex', flexDirection: 'column', padding: '0px 15px' }}>
-          <CardContent sx={{ flex: '1 0 auto' }}>
-            <Typography variant='h5' component="h3">
-              Title
+      <Card
+        variant="outlined"
+        sx={{ display: "flex", justifyContent: "space-between" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            padding: "0px 15px",
+          }}
+        >
+          <CardContent sx={{ flex: "1 0 auto" }}>
+            <Typography variant="h5" component="h3">
+              {state.song.title}
             </Typography>
-            <Typography variant='subtitle1' component="p" sx={{ color: 'text.primary' }}>
-              Artist
+            <Typography
+              variant="subtitle1"
+              component="p"
+              sx={{ color: "text.primary" }}
+            >
+              {state.song.artist}
             </Typography>
           </CardContent>
-          <div style={{ display: 'flex', alignItems: 'center', paddingLeft: theme.spacing(1), paddingRight: theme.spacing(1) }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              paddingLeft: theme.spacing(1),
+              paddingRight: theme.spacing(1),
+            }}
+          >
             <IconButton>
               <SkipPrevious />
             </IconButton>
-            <IconButton>
-              <PlayArrow sx={{ height: 38, width: 38 }} />
+            <IconButton onClick={handleToggleClick}>
+              {state.isPlaying ? (
+                <Pause sx={{ height: 38, width: 38 }} />
+              ) : (
+                <PlayArrow sx={{ height: 38, width: 38 }} />
+              )}
             </IconButton>
             <IconButton>
               <SkipNext />
             </IconButton>
-            <Typography variant='subtitle1' component="p" sx={{ color: 'text.primary' }}>
+            <Typography
+              variant="subtitle1"
+              component="p"
+              sx={{ color: "text.primary" }}
+            >
               00:01:30
             </Typography>
           </div>
-          <Slider type="range" min={0} max={1} step={0.01} sx={{ color: 'text.alt' }} />
+          <Slider
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            sx={{ color: "text.alt" }}
+          />
         </div>
-        <CardMedia component="img" image="https://i.scdn.co/image/ab67616d00001e02ccba76fad1c624c09e72bc78" sx={{ width: 150 }} />
+        <CardMedia
+          component="img"
+          image={state.song.thumbnail}
+          sx={{ width: 150 }}
+        />
       </Card>
       <QueuedSongList />
     </>
-  )
+  );
 }
