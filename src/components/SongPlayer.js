81eb10a8d4438231e4731c98aabf824b228687ac
Stephen Dalton <daltsdev@icloud.com>
2022-11-14 22:22:33 +0000

    feat: :sparkles: add react player functionality to play a song

--- src/components/SongPlayer.js
+++ src/components/SongPlayer.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
 import { useTheme } from "@mui/material/styles";
 import { PlayArrow, SkipNext, SkipPrevious, Pause } from "@mui/icons-material";
 import {
@@ -14,13 +14,44 @@ import QueuedSongList from "./QueuedSongList";
 import { SongContext } from "../App";
 import { useQuery } from "@apollo/react-hooks";
 import { GET_QUEUED_SONGS } from "../graphql/queries";
+import ReactPlayer from "react-player";
 
 export default function SongPlayer() {
   const { data } = useQuery(GET_QUEUED_SONGS);
+  const reactPlayerRef = useRef();
   const { state, dispatch } = useContext(SongContext);
+  const [played, setPlayed] = useState(0);
+  const [seeking, setSeeking] = useState(false);
+  const [playedSeconds, setPlayedSeconds] = useState(0);
+  const [positionInQueue, setPositionInQueue] = useState(0);
   const theme = useTheme();
+
+  useEffect(() => {
+    const SongIndex = data.queue.findIndex((song) => song.id === state.song.id);
+
+    return () => {
+      second;
+    };
+  }, [state.song.id]);
+
   const handleToggleClick = () => {
     dispatch(state.isPlaying ? { type: "PAUSE_SONG" } : { type: "PLAY_SONG" });
+  };
+
+  const handleProgressChange = (event, newValue) => {
+    setPlayed(newValue);
+  };
+
+  const handleSeekMouseDown = () => {
+    setSeeking(true);
+  };
+  const handleSeekMouseUp = () => {
+    setSeeking(false);
+    reactPlayerRef.current.seekTo(played);
+  };
+
+  const formatDuration = (seconds) => {
+    return new Date(seconds * 1000).toISOString().substr(11, 8);
   };
   return (
     <>
@@ -83,17 +114,33 @@ export default function SongPlayer() {
               component="p"
               sx={{ color: "text.primary" }}
             >
-              00:01:30
+              {formatDuration(playedSeconds)}
             </Typography>
           </Box>
           <Slider
             type="range"
+            value={played}
             min={0}
             max={1}
             step={0.01}
+            onChange={handleProgressChange}
+            onMouseDown={handleSeekMouseDown}
+            onMouseUp={handleSeekMouseUp}
             sx={{ color: "text.alt" }}
           />
         </Box>
+        <ReactPlayer
+          onProgress={({ played, playedSeconds }) => {
+            if (!seeking) {
+              setPlayed(played);
+              setPlayedSeconds(playedSeconds);
+            }
+          }}
+          hidden
+          url={state.song.url}
+          playing={state.isPlaying}
+          ref={reactPlayerRef}
+        />
         <CardMedia
           component="img"
           image={state.song.thumbnail}
